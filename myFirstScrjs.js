3b070eac47ccf0371ef2a2ccd091a5785393070a
ThemiyaL <themiyavliyanage@gmail.com>
2022-05-18 10:30:04 -0700

    changes

--- myFirstScrjs.js
+++ myFirstScrjs.js
@@ -4,3 +4,18 @@ console.log("my first log!");
 console.log(document);
 console.log(document.getElementById("myFirstForm"));
 
+var form = console.log(document.getElementById("myFirstForm"));
+var goBTN = console.log(document.getElementById("go"));
+
+goBTN.addEventListener("click", doWhatIWant);
+
+function doWhatIWant(){
+    console.log("this is my this: ");
+    console.log(this);
+    console.log("this is my event");
+    console.log(event);
+    console.log("this is my form");
+    console.log(form);
+    console.log("these are my form elements");
+    console.log(form.elements[1].value);
+}
